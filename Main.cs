508d30466926eb81c872a2939aaa1158d5b5caf1
khoido2003 <khoizpro31@gmail.com>
2025-03-09 11:10:06 +0700

    Fix indentation

--- Main.cs
+++ Main.cs
@@ -3,29 +3,29 @@ using Godot;
 
 public partial class Main : Node
 {
-	[Export]
-	public PackedScene MobScene { get; set; }
+    [Export]
+    public PackedScene MobScene { get; set; }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() { }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() { }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) { }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
 
-	private void OnMobTimerTimeout()
-	{
-		// Create new instance of mob scene
-		Mob mob = MobScene.Instantiate<Mob>();
+    private void OnMobTimerTimeout()
+    {
+        // Create new instance of mob scene
+        Mob mob = MobScene.Instantiate<Mob>();
 
-		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+        var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
 
-		// Give a random offset
-		mobSpawnLocation.ProgressRatio = GD.Randf();
+        // Give a random offset
+        mobSpawnLocation.ProgressRatio = GD.Randf();
 
-		Vector3 playerPosition = GetNode<Player>("Player").Position;
+        Vector3 playerPosition = GetNode<Player>("Player").Position;
 
-		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+        mob.Initialize(mobSpawnLocation.Position, playerPosition);
 
-		AddChild(mob);
-	}
+        AddChild(mob);
+    }
 }
